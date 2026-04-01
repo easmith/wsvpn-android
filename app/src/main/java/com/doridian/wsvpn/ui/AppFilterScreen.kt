61edcb9471da914edd4aaa8d58392989dfbbbaf5
Eugene Smith <easmith@mail.ru>
2026-04-01 14:17:53 +0300

    Review fixes

--- app/src/main/java/com/doridian/wsvpn/ui/AppFilterScreen.kt
+++ app/src/main/java/com/doridian/wsvpn/ui/AppFilterScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Apps
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
